177354b22a3327d1cd9d176bb77413d4c1103bc7
sgiannouk <s.p.giannoukakos@hotmail.com>
2021-02-17 00:37:39 +0100

    exporting the list of the novel transcripts that are being differentially expressed (DTE)

--- Rscripts/diffExpr_DTE.R
+++ Rscripts/diffExpr_DTE.R
@@ -3,41 +3,41 @@
 
 
 args <- commandArgs(TRUE)
-if (length(args) == 7) {
+if (length(args) == 6) {
   # Input filtered matrix (output of step 8)
   matrix <- args[1]
-  # Read annotation file
-  read_annot <- args[2]
   # CSV file used for running TALON
-  input_groups <- args[3]
+  input_groups <- args[2]
   # Output direcotry where all stats will be saves
-  main_outdir <- args[4]
+  main_outdir <- args[3]
   # Minimum transcript counts
-  minFeatureExpr <- as.numeric(args[5])
+  minFeatureExpr <- as.numeric(args[4])
   # Adjusted p-value threshold for differential expression analysis
-  adjPValueThreshold <- as.numeric(args[6])
+  adjPValueThreshold <- as.numeric(args[5])
   # Minimum required log2 fold change for differential expression analysis
-  lfcThreshold <- as.numeric(args[7])
+  lfcThreshold <- as.numeric(args[6])
 } else {
   cat("ERROR - The number of input arguments is not correct...\nEXITING!\n")
   quit()
 }
 
-# matrix <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/filt_talon_abundance.csv"
-# read_annot <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/prefilt_talon_read_annot.tsv"
-# input_groups <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/talon_input.csv"
-# main_outdir <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/diffExpr_analysis"
-# minFeatureExpr <- 10  # Minimum transcript counts
-# adjPValueThreshold <- 0.05
-# lfcThreshold <- 1
+matrix <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/filt_talon_abundance.csv"
+input_groups <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/talon_input.csv"
+main_outdir <- "/Users/stavris/Desktop/Projects/silvia_ont_umc/talon_analysis_reimplementation_3/diffExpr_analysis"
+minFeatureExpr <- 10  # Minimum transcript counts
+adjPValueThreshold <- 0.05
+lfcThreshold <- 2
 
 
 library("readr")
 library("dplyr")
 library("edgeR")
+library("ggpubr")
 library("DRIMSeq")
 library("ggplot2")
 library("reshape")
+library("patchwork")
+library("ggchicklet")
 options(scipen = 999)
 
 
@@ -93,10 +93,10 @@ drimseq <- dmFilter(drimseq,
 # Obtaining the counts after dmFiltering
 filtered_counts <- counts(drimseq)
 
-# Printing several stats
+
 # Printing stats
-print(paste("In total ", length(unique(filtered_counts$feature_id)), 
-            " out of ", length(unique(matfile$feature_id))," genes passed the min. thresholds.",sep=""))
+print(paste("In total ", length(unique(filtered_counts$feature_id)), "/", length(unique(matfile$feature_id)),
+            " transcripts passed the min. threshold of ", minFeatureExpr," counts.",sep=""))
 
 # Creating with design matrix
 if (length(unique(group_samples$batch) == 1)) {
@@ -171,6 +171,11 @@ results_selected <- results_selected[with(results_selected, order(pval, padj)), 
 # Exporting the normalised results table containing the selected features with log2FoldChange greater than 1 and adjusted p value lower than the user-input value
 write.table(results_selected, file=paste(outdir,"/",sampletypevalues[1],"VS",sampletypevalues[2],"_edgeR_topTranscriptsBelow", gsub("[.]", "", adjPValueThreshold), "LFC", gsub("[.]", "", lfcThreshold), ".csv", sep=""), sep="\t", row.names = F, quote=FALSE)
 
+# Export a list of all novel differentially expressed isoforms to proceed for Functional Annotation
+list_of_de_transcripts <- results_selected[,1]  # List of all transcripts that were differentially expressed
+novel_de_transcripts <- grep('TALONT', list_of_de_transcripts, value=TRUE)  # Obtaining only the novel transcripts that were DEd
+write.table(novel_de_transcripts, file=paste(outdir,"/novel_de_transcripts_for_funcAnnotation.tsv", sep=""), sep="\t", row.names = F, col.names = F, quote=FALSE)
+
 
 
 # MA-plot - Drawing the expression levels over the exons to highlight differential exon usage
@@ -206,16 +211,53 @@ colours <- c(noDifference="dimgray", upRegulated="mediumseagreen", downRegulated
 gene <- rep("noDifference", nrow(edger_res_overall))
 gene[logUp[logUp %in% withStat]] <- "upRegulated"
 gene[logDown[logDown %in% withStat]] <- "downRegulated"
-ggplot(edger_res_overall, aes(x=logCPM, y=logFC)) + 
-        geom_point(aes(shape=factor(novelty,  labels=c("Known", "Novel")))) +
-        geom_hline(yintercept = -lfcThreshold, color="indianred3") +
-        geom_hline(yintercept = lfcThreshold, color="mediumseagreen") +
-        theme_bw() +
-        aes(colour=gene) +
-        scale_colour_manual(name="Transcripts", values=colours) +
-        labs(title="MA plot - log(FC) vs. log(CPM) on transcript level data with Known/Novel-transcript indication", x="log(CountsPerMillion)", y="log(FoldChange)", shape="Known/Novel")
+p1 <- ggplot(edger_res_overall, aes(x=logCPM, y=logFC)) + 
+             geom_point(aes(shape=factor(novelty,  labels=c("Known", "Novel")))) +
+             geom_hline(yintercept = -lfcThreshold, color="indianred3") +
+             geom_hline(yintercept = lfcThreshold, color="mediumseagreen") +
+             theme_minimal() +
+             aes(colour=gene) +
+             scale_colour_manual(name="Transcripts", values=colours) +
+             labs(title="MA plot - log(FC) vs. log(CPM) on transcript level data with Known/Novel-transcript indication", 
+                  x="log(CountsPerMillion)", y="log(FoldChange)", shape="Known/Novel") +
+             theme(legend.position = "right")
 ggsave(file=paste(outdir,"/edgeR_MAplot_withTranscriptIndications.png",sep=""), width = 10, height = 6, units = "in", dpi = 1200)
-rm(logUp, logDown, withStat, colours, gene,withNovel)
+
+
+# Obtaining the novelty information of the significant transcript
+significant_overview  <- expr_file[expr_file$annot_transcript_id %in% results_selected$transcript_id,c(8:9)]
+significant_overview$ISM_subtype <- sub("None", "", significant_overview$ISM_subtype)
+significant_overview <- data.frame(paste(significant_overview$transcript_novelty, significant_overview$ISM_subtype, sep=" "))
+# Transform to count table
+significant_overview <- data.frame(table(significant_overview[,1]))
+# Rename ISM to ISM None as it should be
+significant_overview$Var1 <- gsub("ISM.$", "ISM None", significant_overview$Var1)
+# Get the percentage of each group and sort the groups
+significant_overview$perc <-round(significant_overview[ ,2]/sum(significant_overview[ ,2])*100,3)
+significant_overview <- significant_overview[order(-significant_overview$Freq), ]
+significant_overview$Var1 <- factor(significant_overview$Var1, levels=rev(significant_overview$Var1))
+# Choosing colours for each group
+legend_colors <- c("#ae4544", "#d8cb98", "#a4ad6f", "#cc7c3a", "#436f82", "#7c5981", "#cccccc", "#ffcda3", "#ef4f4f")
+
+p2 <- ggplot(significant_overview, aes(x=Var1, y=perc, fill=Var1)) +
+            geom_chicklet(width = .45) +
+             theme_minimal() +
+             coord_flip() +
+             scale_y_continuous(limits= c(0, round(max(significant_overview$perc), digits = -1)), 
+                                breaks=seq(0, round(max(significant_overview$perc), digits = -1),5)) +
+             theme(plot.title = element_text(colour = "#a6a6a4", size=11),
+                   panel.grid.major.y = element_blank(),
+                   panel.border = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.grid.major.x = element_line(size=.1, color="gray78"),
+                   legend.position = "None") +
+             labs(x="", y="Percentage of transcripts") +
+             scale_fill_manual("", values = legend_colors[1:length(significant_overview$Var1)])
+
+figure <- p1 + p2 + plot_layout(ncol = 1, heights=c(1,.25))
+ggsave(file=paste(outdir,"/edgeR_MAplot_withTranscriptIndicationsNgroupSun.png",sep=""), width = 10, height = 6, units = "in", dpi = 1200)
+rm(logUp, logDown, withStat, colours, gene,withNovel, p1, p2)
 
 # Volcano plot
 # Compute significance, with a maximum of 350 for the p-values set to 0 due to limitation of computation precision
@@ -242,7 +284,7 @@ plot(edger_res_overall$logFC,
      ylab="-log10(adjusted p-value)",
      pch=edger_res_overall$pch, cex=0.4)
 abline(v=0)
-abline(v=c(-1,1), col="brown")
+abline(v=c(-lfcThreshold,lfcThreshold), col="brown")
 abline(h=-log10(adjPValueThreshold), col="brown")
 ## Plot the names of a reasonable number of transcripts, by selecting those begin not only significant but also having a strong effect size
 gn.selected <- (abs(edger_res_overall$logFC)>=lfcThreshold & edger_res_overall$FDR<=adjPValueThreshold)
@@ -250,7 +292,7 @@ text(edger_res_overall$logFC[gn.selected], edger_res_overall$significant[gn.sele
 dev.off()
 rm(genes.to.plot, cols, gn.selected)
 edger_res_overall$significant <- NULL; edger_res_overall$pch  <- NULL; edger_res_overall$transcript_name <- NULL
-rm(is.de,  fit, qlf, transcript_filt_counts, edger_res, edger_res_overall, edgeR_table, results_selected, design)
+rm(is.de,  fit, qlf, transcript_filt_counts, edger_res, edger_res_overall, edgeR_table, design)
 
 
 ### TOP 30 DE TRANSCRIPTS
@@ -275,4 +317,5 @@ ggplot(melt_top30_sigNorm, aes(x = transcript_name, y = value, color = group)) +
   theme(legend.position="bottom") +
   labs(title="Top 30 Significant DE Transcripts", x="Transcripts", y="log10(CPM)")
 ggsave(file=paste(outdir,"/edgeR_top30MostSingificantTranscripts.png",sep=""), width = 10, height = 6, units = "in", dpi = 1200)
-rm(melt_top30_sigNorm, top30_sigNorm)
+rm(melt_top30_sigNorm, top30_sigNorm, n, input_groups, legend_colors, minFeatureExpr, 
+   significant_overview, filtered_counts,drimseq, figure, matfile, matrix)
